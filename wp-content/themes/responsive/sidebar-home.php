d4a99daa136a8c58eda607931c0e311f24ede3ff
ison-hp <ison@hp.com>
2014-04-07 01:27:23 +0800

    beautefy ui

--- wp-content/themes/responsive/sidebar-home.php
+++ wp-content/themes/responsive/sidebar-home.php
@@ -28,28 +28,29 @@ if( !defined( 'ABSPATH' ) ) {
 			<?php if( !dynamic_sidebar( 'home-widget-1' ) ) : ?>
 				<div class="widget-wrapper">
 
-					<div class="widget-title-home"><h3><?php _e( 'Home Widget 1', 'responsive' ); ?></h3></div>
-					<div
-						class="textwidget"><?php _e( 'This is your first home widget box. To edit please go to Appearance > Widgets and choose 6th widget from the top in area 6 called Home Widget 1. Title is also manageable from widgets as well.', 'responsive' ); ?></div>
-
+					<div class="widget-title-home"><h3>
+					本站支持所有浏览设备
+					</h3></div>
+					<div class="textwidget">
+						<img class="aligncenter" src="http://127.0.0.3/wp-content/themes/responsive/core/images/featured-image.png" alt="支持所有设备">
+					</div>
 				</div><!-- end of .widget-wrapper -->
 			<?php endif; //end of home-widget-1 ?>
 
 			<?php responsive_widgets_end(); // responsive after widgets hook ?>
 		</div>
 		<!-- end of .col-300 -->
-
 		<div id="home_widget_2" class="grid col-300">
 			<?php responsive_widgets(); // responsive above widgets hook ?>
 
 			<?php if( !dynamic_sidebar( 'home-widget-2' ) ) : ?>
-				<div class="widget-wrapper">
-
-					<div class="widget-title-home"><h3><?php _e( 'Home Widget 2', 'responsive' ); ?></h3></div>
-					<div
-						class="textwidget"><?php _e( 'This is your second home widget box. To edit please go to Appearance > Widgets and choose 7th widget from the top in area 7 called Home Widget 2. Title is also manageable from widgets as well.', 'responsive' ); ?></div>
-
-				</div><!-- end of .widget-wrapper -->
+				<div class="widget-wrapper"><div class="widget-title-home"><h3>
+				<?php _e( 'Home Widget 2', 'responsive' ); ?>
+				</h3></div>
+				<div class="textwidget">
+					<?php _e( 'This is your second home widget box. To edit please go to Appearance > Widgets and choose 7th widget from the top in area 7 called Home Widget 2. Title is also manageable from widgets as well.', 'responsive' ); ?>
+				</div>
+			</div><!-- end of .widget-wrapper -->
 			<?php endif; //end of home-widget-2 ?>
 
 			<?php responsive_widgets_end(); // after widgets hook ?>
@@ -60,12 +61,12 @@ if( !defined( 'ABSPATH' ) ) {
 			<?php responsive_widgets(); // above widgets hook ?>
 
 			<?php if( !dynamic_sidebar( 'home-widget-3' ) ) : ?>
-				<div class="widget-wrapper">
-
-					<div class="widget-title-home"><h3><?php _e( 'Home Widget 3', 'responsive' ); ?></h3></div>
-					<div
-						class="textwidget"><?php _e( 'This is your third home widget box. To edit please go to Appearance > Widgets and choose 8th widget from the top in area 8 called Home Widget 3. Title is also manageable from widgets as well.', 'responsive' ); ?></div>
-
+				<div class="widget-wrapper"><div class="widget-title-home"><h3>
+				<?php _e( 'Home Widget 3', 'responsive' ); ?>
+				</h3></div>
+					<div class="textwidget">
+					<?php _e( 'This is your third home widget box. To edit please go to Appearance > Widgets and choose 8th widget from the top in area 8 called Home Widget 3. Title is also manageable from widgets as well.', 'responsive' ); ?>
+					</div>
 				</div><!-- end of .widget-wrapper -->
 			<?php endif; //end of home-widget-3 ?>
 
