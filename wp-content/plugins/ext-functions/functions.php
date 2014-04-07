9af9b645b967d154ae54af066837f42bda2b9dc8
ison-hp <ison@hp.com>
2014-04-07 15:21:39 +0800

    add ip and weath in home page

--- wp-content/plugins/ext-functions/functions.php
+++ wp-content/plugins/ext-functions/functions.php
@@ -60,23 +60,7 @@ function getRealIP()
 
 function getIP() 
 {
-	$ipaddress = '';
-     if ($_SERVER['HTTP_CLIENT_IP'])
-         $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-     else if($_SERVER['HTTP_X_FORWARDED_FOR'])
-         $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-     else if($_SERVER['HTTP_X_FORWARDED'])
-         $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-     else if($_SERVER['HTTP_FORWARDED_FOR'])
-         $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-     else if($_SERVER['HTTP_FORWARDED'])
-         $ipaddress = $_SERVER['HTTP_FORWARDED'];
-     else if($_SERVER['REMOTE_ADDR'])
-         $ipaddress = $_SERVER['REMOTE_ADDR'];
-     else
-         $ipaddress = 'UNKNOWN';
-
-     return $ipaddress; 
+     return $_SERVER["REMOTE_ADDR"];
 }
 
 //获取当前完整的带参数的URL
