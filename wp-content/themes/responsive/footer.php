91cacb272f335cccb4e4815a4776851833cbe606
Ison <ison.chang@gmail.com>
2014-05-15 10:09:08 +0800

    add baidu tongji code

--- wp-content/themes/responsive/footer.php
+++ wp-content/themes/responsive/footer.php
@@ -85,5 +85,11 @@ $responsive_options = responsive_get_options();
 <?php responsive_footer_after(); ?>
 
 <?php wp_footer(); ?>
+
+<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F253f9cddb74fdc2705f3da3f36c2906c' type='text/javascript'%3E%3C/script%3E"));
+</script>
+
 </body>
 </html>
