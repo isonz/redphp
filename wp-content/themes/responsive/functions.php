f296bd781d56ef30d6b31fcf331e9a86e5e4e32e
Ison <ison.chang@gmail.com>
2014-06-11 10:51:19 +0800

    close update

--- wp-content/themes/responsive/functions.php
+++ wp-content/themes/responsive/functions.php
@@ -45,4 +45,14 @@ function sc_note( $atts, $content = null ) {
 	return '';
 }
 
+
+// Remove upgrade tip messages.////////修改后台显示更新的代码，，，，
+add_filter('pre_site_transient_update_core',    create_function('$a', "return null;")); // 关闭核心提示
+add_filter('pre_site_transient_update_plugins', create_function('$a', "return null;")); // 关闭插件提示
+add_filter('pre_site_transient_update_themes',  create_function('$a', "return null;")); // 关闭主题提示
+remove_action('admin_init', '_maybe_update_core');    // 禁止 WordPress 检查更新
+remove_action('admin_init', '_maybe_update_plugins'); // 禁止 WordPress 更新插件
+remove_action('admin_init', '_maybe_update_themes');  // 禁止 WordPress 更新主题
+
+
 ?>
